ddf58532d9a994672222ac2c046ddb141b7e52e2
Rafael Ribeiro Carvalho <rafaelcar203@gmail.com>
2023-10-30 23:14:03 -0300

    Implementing SellerDaoJDBC

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -11,5 +12,6 @@ public interface SellerDao {
 	void deleteById(int id);
 	Seller findById(int id);
 	List<Seller> findAll();
+	List<Seller> findByDepartment(Department department);
 	
 }
