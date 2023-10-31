9b844b0d8781905fb03b1ada0939cb906bc37d35
Rafael Ribeiro <rafaelcar203@gmail.com>
2023-10-31 10:00:22 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,20 +16,17 @@ public class Program {
 		
 		System.out.println("=== TEST 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 
 		System.out.println("=== TEST 2: seller findByDepartment =====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		
 		for(Seller obj : list){
 			System.out.println(obj);
 		}
 
 		System.out.println("=== TEST 3: seller findAll =====");
 		list = sellerDao.findAll();
-		
 		for(Seller obj : list){
 			System.out.println(obj);
 		}
@@ -38,5 +35,11 @@ public class Program {
 		Seller newSeller = new Seller( "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Iserted! New id = " + newSeller.getId());
+
+		System.out.println("=== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 }
